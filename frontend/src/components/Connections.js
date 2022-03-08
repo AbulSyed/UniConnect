f13b88eeaec0041b3a5a371a7087033dea15a3a8
Abul Syed <syed1234@hotmail.co.uk>
2022-03-08 18:16:34 +0000

    connections

--- frontend/src/components/Connections.js
+++ frontend/src/components/Connections.js
@@ -4,7 +4,55 @@ import '../styles/connections.scss'
 const Connections = () => {
     return ( 
         <div className="connections">
-            connections
+            <div className="container">
+                <h1 className="heading">Connections</h1>
+                <ul className="connectionsList">
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                    <li className="connectionItem">
+                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
+                        <span>Abul</span>
+                    </li>
+                </ul>
+            </div>
         </div>
      );
 }
