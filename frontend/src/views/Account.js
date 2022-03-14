d79ae938ee36bb29246d5c6752b7c2b4730f4625
Abul Syed <syed1234@hotmail.co.uk>
2022-03-14 15:31:56 +0000

    account page

--- frontend/src/views/Account.js
+++ frontend/src/views/Account.js
@@ -12,8 +12,8 @@ const Account = () => {
             <div className="account">
                 <div className="container">
                     <div className="accountUpper">
-                        <div className="accountCover">
-                            <img className="accountCoverPic" src="https://i.pinimg.com/originals/dd/d5/b2/ddd5b2f90e660eb4f881a59c416f3ac9.jpg" alt="" />
+                        <div className="accountBanner">
+                            <img className="accountBannerPic" src="https://i.pinimg.com/originals/dd/d5/b2/ddd5b2f90e660eb4f881a59c416f3ac9.jpg" alt="" />
                             <img className="accountUserPic" src="https://timelinecovers.pro/facebook-cover/download/photography-city-lights-facebook-cover.jpg" alt="" />
                             <Collections className="uploadIcon" />
                         </div>
