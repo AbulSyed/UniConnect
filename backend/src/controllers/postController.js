682a655996b462443400740f19ad8f80f28dc572
Abul Syed <syed1234@hotmail.co.uk>
2022-03-22 15:01:16 +0000

    post delete & getall user posts

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -5,9 +5,11 @@ const Student = require('../models/student')
 
 // create a post
 const post_create = async (req, res) => {
+    // create new instance of post
     const post = new Post(req.body)
 
     try {
+        // saving to db
         await post.save()
         res.status(201).send(post)
     }catch(err){
@@ -15,6 +17,28 @@ const post_create = async (req, res) => {
     }
 }
 
+// deleting a post
+const post_delete = async (req, res) => {
+    try {
+        await Post.findByIdAndDelete(req.params.id)
+        res.status(200).send('Post deleted')
+    }catch(err){
+        res.status(400).send(err)
+    }
+}
+
+// get all posts by user to be displayed on user account
+const post_account = async (req, res) => {
+    try {
+        const posts = await Post.find({ studentId: req.params.id })
+        res.status(200).send(posts)
+    }catch(err){
+        res.status(400).send(err)
+    }
+}
+
 module.exports = {
     post_create,
+    post_delete,
+    post_account,
 }
--- backend/src/routes/postRoute.js
+++ backend/src/routes/postRoute.js
@@ -5,5 +5,7 @@ const postController = require('../controllers/postController')
 // post route endpoints
 // localhost:8080/api/posts
 router.post('/posts', postController.post_create)
+router.delete('/posts/:id', postController.post_delete)
+router.get('/posts/account/:id', postController.post_account)
 
 module.exports = router
