af1e50eaa5aba9bf8d10530af88e2110bab7c836
Abul Syed <syed1234@hotmail.co.uk>
2022-03-28 15:43:10 +0100

    connections

--- frontend/src/components/Connections.js
+++ frontend/src/components/Connections.js
@@ -1,24 +1,44 @@
 // Styles
 import '../styles/connections.scss'
 
+import { Link } from 'react-router-dom'
+import { useState, useContext, useEffect } from 'react'
+import api from '../axios/api'
+import { Context as AuthContext } from '../context/AuthContext'
+
 const Connections = () => {
+    const { state } = useContext(AuthContext)
+    const [connections, setConnections] = useState([])
+
+    const getConnections = async () => {
+        try {
+            const res = await api.get(`/students/connections/${state.student._id}`)
+            setConnections(res.data)
+        }catch(err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getConnections()
+    }, [])
+
     return ( 
         <div className="connections">
             <div className="contain">
                 <h1 className="heading">Connections</h1>
                 <ul className="connectionsList">
-                    <li className="connectionItem">
-                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
-                        <span>Abul</span>
-                    </li>
-                    <li className="connectionItem">
-                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
-                        <span>Abul</span>
-                    </li>
-                    <li className="connectionItem">
-                        <img className="connectionImg" src="https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg" alt="" />
-                        <span>Abul</span>
-                    </li>
+                    {   connections.length ? 
+                        connections.map(connection => (
+                            <Link key={ connection._id } to={ `/account/${connection._id}` } className="link">
+                                <li className="connectionItem">
+                                    <img className="connectionImg" src={ connection.studentImage } alt="" />
+                                    <span>{ connection.name }</span>
+                                </li>
+                            </Link>
+                        ))
+                        : <p className="altText">No connections</p>
+                    }
                 </ul>
             </div>
         </div>
