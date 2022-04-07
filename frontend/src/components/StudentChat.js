f5d24674be41e8c0e01d4f6bf086629a81b5f773
Abul Syed <syed1234@hotmail.co.uk>
2022-04-07 17:05:56 +0100

    groupchat hack

--- frontend/src/components/StudentChat.js
+++ frontend/src/components/StudentChat.js
@@ -5,27 +5,50 @@ import api from '../axios/api'
 
 const StudentChat = ({ chat }) => {
     const [student, setStudent] = useState([])
+    const [student2, setStudent2] = useState([])
     const { state } = useContext(AuthContext)
 
-    const friendId = chat.students.find(curr => curr !== state.student._id)
+    const friendsId = chat.students.filter(curr => curr !== state.student._id)
 
     const getStudent = async (id) => {
         try {
             const res = await api.get(`/students/${id}`)
             setStudent(res.data)
+            console.log(res.data)
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    const getStudent2 = async (id) => {
+        try {
+            const res = await api.get(`/students/${id}`)
+            setStudent2(res.data)
+            console.log(res.data)
         }catch(err){
             console.log(err)
         }
     }
 
     useEffect(() => {
-        getStudent(friendId)
+        getStudent(friendsId[0])
+        getStudent2(friendsId[1])
     }, [])
 
     return ( 
         <div className="studentChat">
-            <img className="studentChatImg" src={ student.studentImage } />
-            <span>{ student.name }</span>
+            {
+                student2.length == 0 ? <>
+                    <img className="studentChatImg" src={ student.studentImage } />
+                    <span>{ student.name }</span>
+                </> : 
+                <>
+                    <img className="studentChatImg" src={ student.studentImage } />
+                    <img className="studentChatImg" src={ student2.studentImage } />
+                    <span>{ student.name } &amp; { student2.name }</span>
+                </>
+            }
+            
         </div>
     );
 }
