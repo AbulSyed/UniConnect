b530849d4de9f8729d9f9c2ac57c84faf911259f
Abul Syed <syed1234@hotmail.co.uk>
2022-05-04 12:31:02 +0100

    heroku api

--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -3,8 +3,13 @@ import axios from 'axios'
 /*  
   setting up baseURL
   so we dont have to repeat the API URL everytime in our code
+  also useful when we deploy, so just change API url in 1 place
 */
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/api'
+  // dev env
+  // baseURL: 'http://localhost:8080/api',
+
+  // production env
+  baseURL: 'https://syed-uniconnect-api.herokuapp.com/api',
 })
